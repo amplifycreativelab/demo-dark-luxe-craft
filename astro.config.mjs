44dc5b89e02858f4ebde255a176f1401ee557bde
Spaghetti <stefanomocci92@gmmail.com>
2025-12-26 18:19:10 +0800

    Deploy to demo-dark-luxe-craft

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://amplifycreativelab.github.io",
-  base: "/demo-local-trader/",
+  base: "/demo-dark-luxe-craft/",
 
   vite: {
     plugins: [tailwindcss()]
